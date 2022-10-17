e6e0121adda1929c7d153f3b94d22e8f3ff97c5f
QuantumK <quantumk9@hotmail.com>
2022-10-17 20:52:28 +0100

    add more javascript functionality

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
-let topic = "HTML";
 let topics = ["HTML", "CSS", "Git", "JavaScript"];
+let randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let i = 0; i < topics.length; i++) {
-  console.log(topics[i]);
+function listTopics() {
+  for (let i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+  }
 }
 
-// if (topic === "HTML") {
-//   console.log("Let's study HTML!");
-// } else if (topic === "CSS") {
-//   console.log("Let's study CSS!");
-// } else if (topic === "Git") {
-//   console.log("Let's study Git!");
-// } else if (topic === "JavaScript") {
-//   console.log("Let's study JavaScript!");
-// } else {
-//   console.log("Please try again!");
-// }
+function selectTopic(topic) {
+  if (topic === "HTML") {
+    console.log("Let's study HTML!");
+  } else if (topic === "CSS") {
+    console.log("Let's study CSS!");
+  } else if (topic === "Git") {
+    console.log("Let's study Git!");
+  } else if (topic === "JavaScript") {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log("Please try again!");
+  }
+}
+
+console.log("Here are the topics we learned through the PreWork: ");
+listTopics();
+console.log("Which topic should we study first? ");
+selectTopic(randomTopic);
